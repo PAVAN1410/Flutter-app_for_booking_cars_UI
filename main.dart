d7507f16b42b92215eb095546d166fcf002ec09f
pavan <56758401+PAVAN1410@users.noreply.github.com>
2020-03-31 12:53:26 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'app_screenns/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Exploring UI Widgets',
+    debugShowCheckedModeBanner: false,
+    color: Colors.yellow,
+    home: Home))
+      }
